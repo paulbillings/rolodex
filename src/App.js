ba7639406bdcfd0b1fa6d51cefebc310a9608c75
Paul Billings <paulbillings101@yahoo.co.uk>
2021-01-04 21:07:51 +0000

    added componentDidMount function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-     characters: [
-       {
-         name: 'Frankenstein',
-         id: 'c1a1'
-       },
-       {
-        name:  'Dracula',
-        id: 'd2a1'
-      },
-      {
-        name: 'Zombie',
-        id: 'e3a1'
-      },
-     ]
+     characters: []
     };
+  }
+
+  // const [celebrities, setCelebs] = useState([]);
+
+  // const API_KEY = process.env.REACT_APP_RAPIDAPI_KEY;
+
+  // const celebrityPeople = async () => {
+  //   const response = await fetch(url, {
+  //     method: "GET",
+  //     headers: {
+  //       "x-rapidapi-key": API_KEY,
+  //       "x-rapidapi-host": "celebrity-bucks.p.rapidapi.com",
+  //     },
+  //   });
+  //   const celebrities = await response.json();
+  //   setCelebs(celebrities.Birthdays);
+  // };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ characters: users }))
   }
   
   render() {
